9a1f4a6a6263c3615c0883e7b4708c7809cdd9ab
Chesqoo <francisco.j.moralessp@gmail.com>
2022-01-02 19:11:44 +0100

    Generacion de Java lista

---
+++ Debugging/PlantillaGeneracionJava.java
@@ -0,0 +1,28 @@
+﻿
+import java.awt.*;
+import java.awt.event.*; 
+
+public class Framev_VentanaPrin extends Frame impements ActionListener{ 
+
+private Dialogv_VentanaSec ventanav_VentanaSec;
+private Button Boton;
+private MenuItem Item1;
+
+//Aqui va el constructor
+
+//Aqui va la funcion actionPerformed
+}
+
+public class Dialogv_VentanaSec extends Dialog impements ActionListener{ 
+
+private Button Atras;
+private Button Salir;
+
+//Aqui va el constructor
+
+//Aqui va la funcion actionPerformed
+}
+
+
+
+
